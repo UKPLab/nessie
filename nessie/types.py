d11871d9f998ba01aa2bcdb6c9f95a48e31a09ef
Jan-Christoph Klie <git@mrklie.com>
2022-02-01 16:39:48 +0100

    Add StringArray2D

--- nessie/types.py
+++ nessie/types.py
@@ -4,4 +4,6 @@ import awkward as ak
 import numpy.typing as npt
 
 StringArray = Union[List[str], npt.NDArray[str], ak.Array]
+StringArray2D = Union[List[List[str]], npt.NDArray[str], ak.Array]
+
 RaggedStringArray = Union[List[List[str]], npt.NDArray[StringArray], ak.Array]
