bd1adfba262261df5c6b8ed809236cba14d8d0ee
Jan-Christoph Klie <git@mrklie.com>
2022-02-01 11:50:12 +0100

    Remove SVM classifier

--- nessie/models/text/svm_text_classifier.py
+++
@@ -1,9 +0,0 @@
-from sklearn.svm import SVC
-
-from nessie.models.featurizer import SentenceEmbedder
-from nessie.models.text.sklean_text_classifier import SklearnTextClassifier
-
-
-class SvmTextClassifier(SklearnTextClassifier):
-    def __init__(self, embedder: SentenceEmbedder):
-        super().__init__(lambda: SVC(probability=True), embedder)
